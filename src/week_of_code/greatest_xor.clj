91e857952c53654d3dbfe857dd9a066dce79397a
Edwin Dalorzo <edwin@dalorzo.com>
2017-01-11 17:28:29 -0600

    Improves the Great XOR algorithm to just calculate the total count instead of keep the data into a vector and then count the items

--- src/week_of_code/greatest_xor.clj
+++ src/week_of_code/greatest_xor.clj
@@ -7,18 +7,23 @@
 
 ;(map #(vec [% (bit-xor % 26)]) (range 1 26))
 
-(defn expand [a n]
-  (for [k (range a (* a 2)) 
-        :let [x (bit-xor k n)]
-        :while (> x n)]
-    k))
+(defn- expand [a n]
+  (let [limit (* a 2)]
+    (loop [k a total 0]
+      (if (< k limit)
+        (let [x (bit-xor k n) k (inc k)]
+          (if (> x n)
+            (recur k (inc total))
+            (recur k total)))
+        total))))
 
 (defn max-xor [n]
-  (loop [k 0 answer []]
+  (loop [k 0 answer 0]
     (let [a (bit-shift-left 1 k)]
       (if (< a n)
-        (let [x (bit-xor a n)]
+        (let [x (bit-xor a n) k (inc k)]
           (if (> x n)
-            (recur (inc k) (into answer (expand a n)))
-            (recur (inc k) answer)))
+            (recur k (+ answer (expand a n)))
+            (recur k answer)))
         answer))))
+
