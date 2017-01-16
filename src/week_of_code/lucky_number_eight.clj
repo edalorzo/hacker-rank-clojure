28385921f63f92eef0915581916439adbeb39050
Edwin Dalorzo <edwin@dalorzo.com>
2017-01-15 22:20:36 -0600

    Working really hard to try to make this work

--- src/week_of_code/lucky_number_eight.clj
+++ src/week_of_code/lucky_number_eight.clj
@@ -1,35 +1,54 @@
 (ns week-of-code.lucky-number-eight)
 
-(defn get-subseqs [x xs n]
-    (loop [xs xs found []]
-        (if (>= (count xs) n)
-          (let [sub (apply str (concat x (take n xs)))]
-            (recur (rest xs) (conj found sub)))
-          found)))
+(use '[clojure.string :as str :only (join)])
 
-(defn get-seqs [xs n]
-  (loop [[x & xs] xs found []]
-    (if (not-empty xs)
-      (recur xs (concat found (get-subseqs [x] xs n)))
-      found)))
+;(def big-num (vec (map str (apply str (repeat 200000 "968")))))
+;(defn my-reload [] (require '[week-of-code.lucky-number-eight :as woc] :reload))
 
-(defn solve [s]
-  (let [xs (map str (vec s)) size (count xs)]
-    (loop [n 1 found []]
-      (if (<= n size)
-        (let [found (concat found (get-seqs xs n))]
-          (recur (inc n) found))
-        (concat xs found)))))
-      
-    
-    
-    
-    
-    
-    
-    
-    
-      
+(defn sum [a b]
+  (let [a (mod a 1000000007)
+        b (mod b 1000000007)
+        c (mod (+ a b) 1000000007)]
+    c))
+
+(defn divisible-by-eight? [xs]
+  (if (not-empty xs)
+    (let [divides? (fn [x y] (zero? (mod y x)))]
+      (->> xs
+           (str/join)
+           (Integer/parseInt)
+           (divides? 8)))
+    false))
+
+(defn get-combinations [tail n]
+  {:pre [(> n 0)]}
+  (let [sub (str/join (take n tail))]
+    (when (= (count sub) n)
+      (lazy-seq (cons sub (get-combinations (rest tail) n))))))
+
+
+(defn get-subsequence [head tail size]
+  {:pre [(>= (count tail) size)]}
+  (if (> size 0)
+    (let [combs (get-combinations (rest tail) size)
+          seqs (map #(str/join (concat head %)) combs)
+          head (str head (first tail))
+          tail (rest tail)]
+        (lazy-seq (concat seqs (get-subsequence head tail (dec size)))))
+    (lazy-seq (cons head nil))))
+
+(defn get-sequence 
   
+  ([tail] 
+   (let [head (str (first tail))
+         tail (rest tail)
+         size (count tail)]
+     (get-sequence head tail size)))
   
-  
+  ([head tail size]
+   (when (>= size 0)
+     (let [this (get-subsequence head tail size)
+           that (get-sequence head tail (dec size))]
+       (concat this that)))))
+
+
