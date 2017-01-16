24f9ba5cb9ac2980c814d3ec921d952140a60812
Edwin Dalorzo <edwin@dalorzo.com>
2017-01-16 01:25:56 -0600

    I'm getting very close. Fails some tests due to time complexity

--- src/week_of_code/lucky_number_eight.clj
+++ src/week_of_code/lucky_number_eight.clj
@@ -20,35 +20,28 @@
            (divides? 8)))
     false))
 
-(defn get-combinations [tail n]
-  {:pre [(> n 0)]}
-  (let [sub (str/join (take n tail))]
-    (when (= (count sub) n)
-      (lazy-seq (cons sub (get-combinations (rest tail) n))))))
+(defn get-combinations [head tail]
+  (if (> (count tail) 1)
+    (let [[x & xs] tail h (str head x)]
+      (lazy-seq (concat [(cons h xs)] (get-combinations head xs))))))
 
-
-(defn get-subsequence [head tail size]
-  {:pre [(>= (count tail) size)]}
-  (if (> size 0)
-    (let [combs (get-combinations (rest tail) size)
-          seqs (map #(str/join (concat head %)) combs)
-          head (str head (first tail))
-          tail (rest tail)]
-        (lazy-seq (concat seqs (get-subsequence head tail (dec size)))))
-    (lazy-seq (cons head nil))))
-
-(defn get-sequence 
-  
+(defn get-sequences
   ([tail] 
-   (let [head (str (first tail))
-         tail (rest tail)
-         size (count tail)]
-     (get-sequence head tail size)))
+   (get-sequences "" tail))
+  ([head tail] 
+   (if-let [seed (get-combinations head tail)]
+    (let [subseqs (filter #(> (count (rest %)) 1) seed)
+          subseqs (mapcat #(get-sequences (first %) (rest %)) subseqs)]
+      (lazy-seq (concat seed subseqs))))))
   
-  ([head tail size]
-   (when (>= size 0)
-     (let [this (get-subsequence head tail size)
-           that (get-sequence head tail (dec size))]
-       (concat this that)))))
+(defn expand-sequence [tail]
+  (let [expand-seq (fn [head tail] (map #(str head %) tail))
+        sequences (get-sequences tail)
+        combinations (mapcat #(expand-seq (first %) (rest %)) sequences)
+        combinations (concat (map str (vec tail)) combinations)
+        divisible? (memoize divisible-by-eight?)]
+    (filter #(divisible? (take-last 3 %)) combinations)))
 
-
+(defn solve [tail]
+  (let [results (expand-sequence tail)]
+    (reduce (fn [a b] (sum a 1)) 0 results)))
